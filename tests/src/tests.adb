5e6d04204473cff9d8f6736d8aa5d9f3d0756dc8
Emanuele Zarfati <zarfati.tecno@gmail.com>
2024-01-05 20:04:09 +0100

    Add support to qemu emulation for tests

--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -1,4 +1,10 @@
+with Ada.Text_IO;
+
 procedure Tests is
+   use Ada.Text_IO;
 begin
-   null;
+
+   --  Put_Line only works when debugging with semihosting
+   Put_Line ("Welcome to STM32L0xx Cmsis library's tests");
+
 end Tests;
